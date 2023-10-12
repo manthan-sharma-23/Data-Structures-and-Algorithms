5f1700bc17e8f66277e0b34cfe444aa4cebaa869
Manthan Sharma <143496678+manthan-sharma-23@users.noreply.github.com>
2023-10-12 14:24:33 +0530

    insertion sort complete

---
+++ java/sorting/insertion_sort.java
@@ -0,0 +1,34 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class insertion_sort {
+    public static void main(String[] args) {
+        int arr[] = { 2, 3, 1, 5, 4, -2, -4 };
+        arr = insertion_sort_(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    static void swap(int arr[], int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static int[] insertion_sort_(int arr[]) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return arr;
+    }
+}
