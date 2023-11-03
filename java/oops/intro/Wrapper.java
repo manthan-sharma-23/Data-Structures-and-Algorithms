4158ea4f11b3027fba9fbca0823ebebea27dd247
Manthan Sharma <143496678+manthan-sharma-23@users.noreply.github.com>
2023-11-03 19:46:56 +0530

    intro to classes and objects

---
+++ java/oops/intro/Wrapper.java
@@ -0,0 +1,10 @@
+package intro;
+
+public class Wrapper {
+    public static void main(String[] args) {
+        int a = 10;
+        Integer num = 45; // makes an object instead of value pointing or something like that
+        num.toString();
+        System.out.println(num);
+    }
+}
--- java/oops/intro/main.java
+++ java/oops/intro/main.java
@@ -6,16 +6,31 @@ public class main {
         // Student[] students=new Student[10];
         // constructor defines what to be done when the object gets created
 
-        Student manthan=new Student();
-       
-        manthan.rno=13;
-        manthan.name="Manthan Sharma";
-        manthan.marks=92.5f;
+        Student manthan = new Student(35, "Manthan Sharma", 88.5f);
+        // Student() --> special type of function in a class called constructor
 
-        System.out.println(manthan.rno);
-        System.out.println(manthan.name);
-        System.out.println(manthan.marks);
+        // System.out.println(manthan.rno);
+        // System.out.println(manthan.name);
+        // System.out.println(manthan.marks);
 
+        // manthan.rno = 13;
+        // manthan.name = "Manthan Sharma";
+        // manthan.marks = 92.5f;
+
+        // System.out.println("\n" + manthan.rno);
+        // System.out.println(manthan.name);
+        // System.out.println(manthan.marks);
+
+        // manthan.greeting();
+
+        // Student someone = new Student();
+
+        // System.out.println(someone.name);
+
+        Student Aditya = new Student("Aditya Khitauliya", manthan);
+        System.out.println(Aditya.name);
+        System.out.println(Aditya.rno);
+        System.out.println(Aditya.marks);
     }
 
 }
@@ -25,4 +40,22 @@ class Student {
     int rno;
     String name;
     float marks;
+
+    void greeting() {
+        System.out.println("Hello there, I am " + this.name);
+    }
+
+    Student(String name, Student other) { // constructor or function overloading
+        this(other.rno,name,other.marks);
+    }
+
+    Student(int roll, String naam, float number) { // constructor
+        this.name = naam;
+        this.rno = roll;
+        this.marks = number;
+    }
+
+    Student() { // constructor
+        this(-1, "Anonymous", 100f);
+    }
 }
