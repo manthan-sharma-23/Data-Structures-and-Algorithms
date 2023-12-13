8668cc00afb8dfcc68cdadf5a3c1266ba54e768c
Manthan Sharma <143496678+manthan-sharma-23@users.noreply.github.com>
2023-12-13 19:22:59 +0530

    new start

---
+++ java/collections/main.java
@@ -0,0 +1,12 @@
+package collections;
+
+import java.util.*;
+
+public class main {
+
+    public static void main(String[] args) {
+        ArrayList<String> studentName=new ArrayList<>();
+
+        System.out.println(studentName);
+    }   
+}
