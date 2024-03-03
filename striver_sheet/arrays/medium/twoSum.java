7d77e3b3f20f637e1d84a94fd7d5b813216ddaaf
Manthan Sharma <143496678+manthan-sharma-23@users.noreply.github.com>
2024-03-03 20:23:30 +0530

    two sum problem

--- striver_sheet/arrays/medium/twoSum.java
+++ striver_sheet/arrays/medium/twoSum.java
@@ -11,13 +11,16 @@ public class twoSum {
 
         int target = 3;
 
-        int[] indexe = twoSumBruteForce(nums, target);
+        int[] indexe = twoSumBetter(nums, target);
 
         System.out.println(Arrays.toString(indexe));
     }
 
+    // O(n2)-- Time
+    // O(1) -- Space
     static int[] twoSumBruteForce(int[] nums, int target) {
         int result[] = new int[2];
+        result[0] = result[1] = -1;
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
@@ -26,6 +29,51 @@ public class twoSum {
                     return result;
                 }
             }
+        }
+        return result;
+    }
+
+    static int[] twoSumBetter(int[] nums, int target) {
+        int[] ans = new int[2];
+        ans[0] = ans[1] = -1;
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int moreNeeded = target - num;
+            if (mpp.containsKey(moreNeeded)) {
+                ans[0] = mpp.get(moreNeeded);
+                ans[1] = i;
+                return ans;
+            }
+
+            mpp.put(nums[i], i);
+
+        }
+
+        return ans;
+
+    }
+
+    //optimal 
+    static int[] twoSumOptimal(int[] nums, int target) {
+        Arrays.sort(nums);
+        int[] result = new int[2];
+        result[0] = result[1] = -1;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == target) {
+                result[0] = left;
+                result[1] = right;
+                return result;
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+
         }
         return result;
     }
