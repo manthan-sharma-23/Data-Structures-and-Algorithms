a9e1c54f9d43589a8c78c499bad59efaa64fbfca
Manthan Sharma <143496678+manthan-sharma-23@users.noreply.github.com>
2024-03-04 02:06:23 +0530

    moore's voting algo

---
+++ striver_sheet/arrays/medium/majorityElement.java
@@ -0,0 +1,68 @@
+package striver_sheet.arrays.medium;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class majorityElement {
+
+    public static void main(String[] args) {
+        int[] nums = { 3, 2, 3 };
+        int it = majorityElementOptimal(nums);
+
+        System.out.println(it);
+    }
+
+    // returns index
+    // O(2n) -> Time
+    // Space != O(1)
+    static int majorityElementBetter(int[] nums) {
+        int n = nums.length;
+        int result = -1;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // arrange the hash map
+        for (int i = 0; i < nums.length; i++) {
+            int value = map.getOrDefault(nums[i], 0);
+            map.put(nums[i], value + 1);
+        }
+
+        System.out.println(map);
+
+        for (Map.Entry<Integer, Integer> it : map.entrySet()) {
+            if (it.getValue() > (n / 2)) {
+                result = it.getKey();
+            }
+        }
+
+        return result;
+    }
+
+    static int majorityElementOptimal(int[] nums) {
+
+        int elmt = 0;
+        int c = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (c == 0) {
+                c = 1;
+                elmt = nums[i];
+            } else if (nums[i] == elmt) {
+                c++;
+            } else {
+                c--;
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == elmt) {
+                count++;
+            }
+        }
+
+        if (count >nums.length / 2)
+            return elmt;
+
+        return -1;
+    }
+}
---
+++ striver_sheet/arrays/medium/sortColors.java
@@ -0,0 +1,26 @@
+package striver_sheet.arrays.medium;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class sortColors {
+    public static void main(String[] args) {
+        int[] colors = { 0, 1, 2, 2, 0, 1, 1, 0 };
+        int[] sortedColors = sortColorsBruteForce(colors);
+        System.out.println(Arrays.toString(sortedColors));
+    }
+
+    static int[] sortColorsBruteForce(int[] colors) {
+        int[] result = new int[colors.length];
+        HashMap<Integer, Integer> colorMap = new HashMap<>();
+        for (int i : colors) {
+            if (colorMap.containsKey(i)) {
+                colorMap.put(i, colorMap.get(i) + 1);
+            } else {
+                colorMap.put(i, 1);
+            }
+        }
+        
+        return result;
+    }
+}
