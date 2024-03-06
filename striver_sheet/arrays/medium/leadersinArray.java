85c1416e301334328cfdc6967dae0e6fcfbaa452
Manthan Sharma <143496678+manthan-sharma-23@users.noreply.github.com>
2024-03-06 11:57:59 +0530

    leaders in an array

---
+++ striver_sheet/arrays/medium/leadersinArray.java
@@ -0,0 +1,50 @@
+package striver_sheet.arrays.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class leadersinArray {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 2, 1 };
+
+        List<Integer> list = leadersinArrayBrute(nums);
+
+        System.out.println(list);
+    }
+
+    static List<Integer> leadersinArrayBrute(int[] nums) {
+        int n = nums.length;
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(nums[n - 1]);
+        for (int i = n - 2; i >= 0; i--) {
+            boolean large = true;
+            for (int j = i+1; j < n; j++) {
+                if (nums[j] >= nums[i]) {
+                    large = false;
+                    break;
+                }
+            }
+            if (large) {
+                list.add(nums[i]);
+            }
+        }
+        return list;
+    }
+
+    static List<Integer> leadersinArrayOptimal(int[] nums) {
+        int n = nums.length;
+        ArrayList<Integer> list = new ArrayList<>();
+        int index = n - 1;
+        int max = Integer.MIN_VALUE;
+
+        while (index >= 0) {
+            if (nums[index] > max) {
+                list.add(nums[index]);
+                max = nums[index];
+            }
+
+            index--;
+        }
+        return list;
+    }
+}
---
+++ striver_sheet/arrays/medium/nextPermutation.java
@@ -0,0 +1,11 @@
+package striver_sheet.arrays.medium;
+
+public class nextPermutation {
+    public static void main(String[] args) {
+        
+    }
+
+    static void nextPermutationBrute(int[] nums){
+        
+    }
+}
