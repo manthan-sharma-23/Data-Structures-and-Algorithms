955af2098b7250724112bb43fb5a3f1ade0cc2c1
Manthan Sharma <manthan2306s@gmail.com>
2024-07-14 21:10:40 +0530

    graph.cpp

---
+++ graphs/lvl0/bfs.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+vector<int> bfsOfGraph(int V, vector<int> adj[])
+{
+    // Code here
+    int vis[V]={0};
+
+    vis[0] = 1;
+
+    queue<int> q;
+    q.push(0);
+
+    vector<int> bfs;
+
+    while (!q.empty())
+    {
+        int node = q.front();
+        q.pop();
+
+        for (auto it : adj[node])
+        {
+            if (!vis[it])
+            {
+                vis[it] = 1;
+                q.push(it);
+            }
+        }
+
+        return bfs;
+    }
+}
---
+++ graphs/lvl0/dfs.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void dfs(int node, vector<int> &ls, vector<int> adj[], int vis[])
+{
+    vis[node] = 1;
+    ls.push_back(node);
+
+    for (auto it : adj[node])
+    {
+        if (!vis[it])
+        {
+            dfs(it, ls, adj, vis);
+        }
+    }
+}
+
+vector<int> dfsonG(int V, vector<int> adj[])
+{
+    int vis[V] = {0};
+
+    int start = 0;
+    vector<int> ls;
+
+    dfs(0, ls, adj, vis);
+
+    return ls;
+}
