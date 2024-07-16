03eed83c95b9785814b929521cf0564665d94ee6
Manthan Sharma <manthan2306s@gmail.com>
2024-07-16 22:11:43 +0530

    rotten oranges

--- arrays/medium/kadane.java
+++ arrays/medium/kadane.java
@@ -71,7 +71,7 @@ public class kadane {
             System.out.print(nums[i] + " ");
         }
 
-        
+
     }
 
 }
--- graphs/lvl1/rotten_oranges.cpp
+++ graphs/lvl1/rotten_oranges.cpp
@@ -4,29 +4,62 @@ using namespace std;
 
 class Solution
 {
-private:
-    vector<vector<int>> convertToAdjList(const vector<vector<int>> &matrix, int V)
+public:
+    int orangesRotting(vector<vector<int>> &grid)
     {
+        int n = grid.size();
+        int m = grid[0].size();
+        int time = 0;
+        int total = 0, count = 0;
 
-        vector<vector<int>> adj(V);
-        for (int i = 1; i < V; i++)
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+        vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        queue<pair<pair<int, int>, int>> q;
+
+        for (int i = 0; i < n; i++)
         {
-            for (int j = 1; j < V; j++)
+            for (int j = 0; j < m; j++)
             {
-                if (matrix[i][j] == 1 && i != j)
+                if (grid[i][j] == 2)
                 {
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+                    q.push({{i, j}, 0});
+                }
+
+                if (grid[i][j] != 0)
+                {
+                    total++;
                 }
             }
         }
 
-        return adj;
-    }
+        while (!q.empty())
+        {
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int t = q.front().second;
+            count++;
 
-public:
-    int orangesRotting(vector<vector<int>> &grid)
-    {
-        vector<vector<int>> v = convertToAdjList(grid, grid.size());
+            time = max(time, t);
+            q.pop();
+
+            visited[row][col] = 2;
+
+            for (auto it : directions)
+            {
+                int nrow = row + it.first;
+                int ncol = col + it.second;
+
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+                    (grid[nrow][ncol] == 1) && (visited[nrow][ncol] != 2))
+                {
+
+                    visited[nrow][ncol] = 2;
+                    q.push({{nrow, ncol}, t + 1});
+                }
+            }
+        }
+
+        return total == count ? time : -1;
     }
 };
