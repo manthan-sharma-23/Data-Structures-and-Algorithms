1333d9112750d5c4a68a4a6add4058b84e8b83ab
Manthan Sharma <manthan2306s@gmail.com>
2024-07-19 03:10:03 +0530

    is cycle dfs

--- graphs/lvl1/is_cyclic_undirected_graph.cpp
+++ graphs/lvl1/is_cyclic_undirected_graph.cpp
@@ -28,8 +28,6 @@ private:
     return false;
   }
 
-
-
 public:
   bool isCycle(int V, vector<int> adj[]) {
     vector<int> vis(V, 0);
@@ -44,3 +42,40 @@ public:
     return false;
   }
 };
+
+class Solution_DFS {
+private:
+  bool isCycleDFS(vector<int> adj[], vector<bool> &visited, int node,
+                  int parent) {
+
+    visited[node] = true;
+
+    for (auto it : adj[node]) {
+      if (it == parent)
+        continue;
+
+      if (visited[it]) {
+        return true;
+      }
+
+      if (isCycleDFS(adj, visited, it, node)) {
+        return true;
+      };
+    }
+
+    return false;
+  }
+
+public:
+  bool isCycle(int V, vector<int> adj[]) {
+    vector<bool> visited(V, false);
+
+    for (int i = 0; i < V; i++) {
+      if (!visited[i] && isCycleDFS(adj, visited, i, -1)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
