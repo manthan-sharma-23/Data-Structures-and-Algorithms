790be528a91b4d8991eda55bf141f1dc56cb9717
Manthan Sharma <manthan2306s@gmail.com>
2024-07-18 22:25:59 +0530

    word ladder bfs 1

---
+++ graphs/lvl1/detonate_a_bomb.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+
+
+public:
+  int maximumDetonation(vector<vector<int>> &bombs) {
+    int detonated_bombs = 0;
+
+    return detonated_bombs;
+  }
+};
--- graphs/lvl1/minimum_distance_0.cpp
+++ graphs/lvl1/minimum_distance_0.cpp
@@ -1,66 +1,124 @@
 #include <bits/stdc++.h>
-#include <climits>
 #include <queue>
 #include <utility>
 #include <vector>
-
 using namespace std;
 
 class Solution {
-
 private:
   vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-  int findNearestZero(int row, int col, const vector<vector<int>> &mat) {
-    int m = mat.size(), n = mat[0].size();
+
+  void findNearest(vector<vector<int>> &distance,
+                   const vector<vector<int>> &matrix, int m, int n) {
     vector<vector<int>> visited(m, vector<int>(n, 0));
 
-    if (mat[row][col] == 0) {
-      return 0;
-    }
-    int min_d = INT_MAX;
-
     queue<pair<pair<int, int>, int>> q;
-    q.push({{row, col}, 0});
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (matrix[i][j] == 0) {
+          visited[i][j] = 1;
+          q.push({{i, j}, 0});
+        } else {
+          visited[i][j] = 0;
+        }
+      }
+    }
 
     while (!q.empty()) {
       int row = q.front().first.first;
       int col = q.front().first.second;
-      int dis = q.front().second;
-
-      if (mat[row][col] == 0) {
-        min_d = min(min_d, dis);
-      }
+      int dist = q.front().second;
 
       q.pop();
+
+      distance[row][col] = dist;
 
       for (auto dir : directions) {
         int nrow = row + dir.first;
         int ncol = col + dir.second;
 
-        if (nrow >= 0 && ncol >= 0 && nrow < m && ncol < n) {
-          if (visited[nrow][ncol] == 0) {
-            visited[nrow][ncol] = 1;
-            q.push({{nrow, ncol}, dis + 1});
-          }
+        if (nrow >= 0 && ncol >= 0 && nrow < m && ncol < n &&
+            visited[nrow][ncol] == 0) {
+          visited[nrow][ncol] = 1;
+          q.push({{nrow, ncol}, dist + 1});
         }
       }
     }
-
-    return min_d < m * n ? min_d : -1;
   }
 
 public:
   vector<vector<int>> updateMatrix(vector<vector<int>> &mat) {
     int m = mat.size(), n = mat[0].size();
-    vector<vector<int>> distance(m, vector<int>(n, -1));
+    vector<vector<int>> distance(m, vector<int>(n, 0));
 
-    for (int i = 0; i < m; i++) {
-      for (int j = 0; j < n; j++) {
-        int el = findNearestZero(i, j, mat);
-        distance[i][j] = el;
-      }
-    }
+    findNearest(distance, mat, m, n);
 
     return distance;
   }
 };
+
+class Solution2 {
+public:
+  vector<vector<int>> updateMatrix(vector<vector<int>> &mat) {
+    queue<pair<int, int>> q;
+    int m = mat.size();
+    int n = mat[0].size();
+
+    vector<vector<int>> visited(m, vector<int>(n, 0));
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (!mat[i][j]) {
+          q.push({i, j});
+          visited[i][j] = 1;
+        }
+      }
+    }
+
+    int dis = 1;
+
+    int row[] = {-1, 0, 1, 0};
+    int col[] = {0, 1, 0, -1};
+    while (!q.empty()) {
+      int size = q.size();
+      for (int i = 1; i <= size; i++) {
+        auto top = q.front();
+        int prevrow = top.first;
+        int prevcol = top.second;
+        q.pop();
+        for (int j = 0; j < 4; j++) {
+          int newrow = prevrow + row[j];
+          int newcol = prevcol + col[j];
+
+          if (newrow >= 0 && newrow < m && newcol >= 0 && newcol < n &&
+              !visited[newrow][newcol]) {
+            q.push({newrow, newcol});
+            mat[newrow][newcol] = dis;
+            visited[newrow][newcol] = 1;
+          }
+        }
+      }
+      dis++;
+    }
+    return mat;
+  }
+};
+
+void print(vector<vector<int>> v) {
+  for (auto it : v) {
+    for (auto i : it) {
+      cout << i << " ";
+    }
+    cout << endl;
+  }
+}
+
+int main() {
+  vector<vector<int>> grid{{0, 1, 1, 0}, {1, 1, 0, 0}, {0, 0, 1, 41}};
+  Solution obj;
+  vector<vector<int>> ans = obj.updateMatrix(grid);
+
+  print(ans);
+  return 0;
+}
