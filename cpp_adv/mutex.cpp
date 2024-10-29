14225b4b2107716c4ba563b61420e1a75b43a685
Manthan Sharma <manthan2306s@gmail.com>
2024-10-29 20:52:37 +0530

    mutex locks

---
+++ cpp_adv/mutex.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+using namespace std;
+
+int ammount = 0;
+
+std::mutex m;
+
+auto addMoney = []()
+{
+    m.lock();
+    ++ammount;
+    cout << ammount << endl;
+    m.unlock();
+};
+
+int main()
+{
+    std::thread t1(addMoney);
+    std::thread t2(addMoney);
+
+    t1.join();
+    t2.join();
+
+    cout << ammount
+         << endl;
+    return 0;
+}
