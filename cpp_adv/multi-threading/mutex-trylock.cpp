fc64fafa8655ac8e079f37832962deeb18699927
Manthan Sharma <manthan2306s@gmail.com>
2024-10-29 21:25:53 +0530

    mutex locking

---
+++ cpp_adv/multi-threading/mutex-trylock.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+int counter = 0;
+std::mutex m;
+
+void iterateCounter()
+{
+    for (int i = 0; i <= 100; i++)
+    {
+        if (m.try_lock())
+        {
+            counter = i;
+            std::cout << "i " << i << std::endl;
+
+            m.unlock();
+        }
+    }
+}
+
+int main()
+{
+    std::thread t1(iterateCounter);
+    std::thread t2(iterateCounter);
+
+    t1.join();
+
+    t2.join();
+
+    std::cout << counter << std::endl;
+    return 0;
+}
