a45202efb6faa0902cf893a0b3921b903e1273c3
Manthan Sharma <manthan2306s@gmail.com>
2024-11-04 18:43:46 +0530

    checksums

--- cpp_adv/multi-threading/mutex-trylock.cpp
+++ cpp_adv/multi-threading/mutex-trylock.cpp
@@ -1,33 +1,29 @@
 #include <iostream>
-#include <thread>
 #include <mutex>
+#include <thread>
 
 int counter = 0;
 std::mutex m;
 
-void iterateCounter()
-{
-    for (int i = 0; i <= 100; i++)
-    {
-        if (m.try_lock())
-        {
-            counter = i;
-            std::cout << "i " << i << std::endl;
+void iterateCounter() {
+  for (int i = 0; i <= 100; i++) {
+    if (m.try_lock()) {
+      counter = i;
+      std::cout << "i " << i << std::endl;
 
-            m.unlock();
-        }
+      m.unlock();
     }
+  }
 }
 
-int main()
-{
-    std::thread t1(iterateCounter);
-    std::thread t2(iterateCounter);
+int main() {
+  std::thread t1(iterateCounter);
+  std::thread t2(iterateCounter);
 
-    t1.join();
+  t1.join();
 
-    t2.join();
+  t2.join();
 
-    std::cout << counter << std::endl;
-    return 0;
+  std::cout << counter << std::endl;
+  return 0;
 }
--- cpp_adv/multi-threading/std-lock.cpp
+++ cpp_adv/multi-threading/std-lock.cpp
@@ -1,6 +1,6 @@
-#include <mutex>
 #include <chrono>
 #include <iostream>
+#include <mutex>
 #include <thread>
 
 using namespace std;
@@ -9,53 +9,48 @@ int X = 0, Y = 0;
 
 std::mutex m1, m2;
 
-void doSomeWorkForSeconds(int seconds) { std::this_thread::sleep_for(std::chrono::seconds(seconds)); }
-
-void increementXY(int &XorY, std::mutex &m, const char *desc)
-{
-    for (int i = 0; i < 5; i++)
-    {
-        m.lock();
-        ++XorY;
-        cout << desc << " " << XorY << endl;
-        m.unlock();
-        doSomeWorkForSeconds(1);
-    }
+void doSomeWorkForSeconds(int seconds) {
+  std::this_thread::sleep_for(std::chrono::seconds(seconds));
 }
 
-void consumeXY()
-{
-    int useCount = 5;
-    int XplusY = 0;
-    while (useCount-- > 0)
-    {
-        int lockResult = std::try_lock(m1, m2);
-        if (lockResult == -1)
-        {
-            if (X != 0 && Y != 0)
-            {
-                --useCount;
-                XplusY += X + Y;
-                X = 0;
-                Y = 0;
-                cout << "XplusY " << XplusY << endl;
-            }
-
-            m1.unlock();
-            m2.unlock();
-        }
-    }
+void increementXY(int &XorY, std::mutex &m, const char *desc) {
+  for (int i = 0; i < 5; i++) {
+    m.lock();
+    ++XorY;
+    cout << desc << " " << XorY << endl;
+    m.unlock();
+    doSomeWorkForSeconds(1);
+  }
 }
 
-int main()
-{
-    std::thread t1(increementXY, std::ref(X), std::ref(m1), "X");
-    std::thread t2(increementXY, std::ref(Y), std::ref(m2), "Y");
-    std::thread t3(consumeXY);
+void consumeXY() {
+  int useCount = 5;
+  int XplusY = 0;
+  while (useCount-- > 0) {
+    int lockResult = std::try_lock(m1, m2);
+    if (lockResult == -1) {
+      if (X != 0 && Y != 0) {
+        --useCount;
+        XplusY += X + Y;
+        X = 0;
+        Y = 0;
+        cout << "XplusY " << XplusY << endl;
+      }
 
-    t1.join();
-    t2.join();
-    t3.join();
+      m1.unlock();
+      m2.unlock();
+    }
+  }
+}
 
-    return 0;
+int main() {
+  std::thread t1(increementXY, std::ref(X), std::ref(m1), "X");
+  std::thread t2(increementXY, std::ref(Y), std::ref(m2), "Y");
+  std::thread t3(consumeXY);
+
+  t1.join();
+  t2.join();
+  t3.join();
+
+  return 0;
 }
