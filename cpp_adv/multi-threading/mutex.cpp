91700af0f9158960dd0c09df1837932c999b4ffa
Manthan Sharma <manthan2306s@gmail.com>
2024-11-04 18:48:40 +0530

    cehcks

--- cpp_adv/multi-threading/mutex.cpp
+++ cpp_adv/multi-threading/mutex.cpp
@@ -1,30 +1,28 @@
 #include <iostream>
-#include <thread>
 #include <mutex>
+#include <thread>
 
+/// haha doing some checks and nothign else dont read !
 using namespace std;
 
 int ammount = 0;
 
 std::mutex m;
 
-auto addMoney = []()
-{
-    m.lock();
-    ++ammount;
-    cout << ammount << endl;
-    m.unlock();
+auto addMoney = []() {
+  m.lock();
+  ++ammount;
+  cout << ammount << endl;
+  m.unlock();
 };
 
-int main()
-{
-    std::thread t1(addMoney);
-    std::thread t2(addMoney);
+int main() {
+  std::thread t1(addMoney);
+  std::thread t2(addMoney);
 
-    t1.join();
-    t2.join();
+  t1.join();
+  t2.join();
 
-    cout << ammount
-         << endl;
-    return 0;
+  cout << ammount << endl;
+  return 0;
 }
