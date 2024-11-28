916f3d2785de11c4a9b82ed3e1261a4eaa850d9f
Manthan Sharma <manthan2306s@gmail.com>
2024-11-29 01:17:48 +0530

    memo and knapsack

---
+++ DP/knapsack/subset_sum.cc
@@ -0,0 +1,55 @@
+#include <vector>
+
+using namespace std;
+
+class MemoizedSolution {
+public:
+  bool memo_knapsack(vector<vector<bool>> &memo, vector<int> &arr, int target,
+                     int n) {
+    if (target == 0)
+      return true;
+    if (n == 0)
+      return false;
+
+    if (memo[target][n] != 0)
+      return memo[target][n];
+
+    if (arr[n - 1] <= target)
+      return memo[target][n] =
+                 memo_knapsack(memo, arr, target - arr[n - 1], n - 1) ||
+                 memo_knapsack(memo, arr, target, n - 1);
+    else
+      return memo[target][n] = memo_knapsack(memo, arr, target, n - 1);
+  }
+
+  bool isSubsetSum(vector<int> &arr, int target) {
+    int n = arr.size();
+    vector<vector<bool>> memo(target + 1, vector<bool>(n + 1, 0));
+
+    return memo_knapsack(memo, arr, target, n);
+  }
+};
+
+class TopDownSolution {
+public:
+  bool isSubsetSum(vector<int> &arr, int target) {
+    int n = arr.size();
+    vector<vector<bool>> memo(n + 1, vector<bool>(target + 1, false));
+
+    for (int i = 0; i <= n; i++) {
+      memo[i][0] = true;
+    }
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= target; j++) {
+        if (arr[i - 1] <= j) {
+          memo[i][j] = memo[i - 1][j - arr[i - 1]] || memo[i - 1][j];
+        } else {
+          memo[i][j] = memo[i - 1][j];
+        }
+      }
+    }
+
+    return memo[n][target];
+  }
+};
---
+++ DP/knapsack/weight-bag.cc
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class MemoizedSolution {
+  int maxProfit(vector<vector<int>> &memo, vector<int> &weights,
+                vector<int> &val, int wt, int n) {
+    if (n == 0 || wt == 0)
+      return 0;
+
+    if (memo[wt][n] != -1)
+      return memo[wt][n];
+
+    if (weights[n - 1] <= wt) {
+      return memo[wt][n] =
+                 max(val[n - 1] + maxProfit(memo, weights, val,
+                                            wt - weights[n - 1], n - 1),
+                     maxProfit(memo, weights, val, wt, n - 1));
+    } else {
+
+      return memo[wt][n] = maxProfit(memo, weights, val, wt, n - 1);
+    }
+  }
+
+  int knapSack(int capacity, vector<int> &val, vector<int> &weights) {
+    // Initialize memo table with dimensions [capacity + 1][val.size() + 1]
+    vector<vector<int>> memo(capacity + 1, vector<int>(val.size() + 1, -1));
+
+    return maxProfit(memo, weights, val, capacity, val.size());
+  }
+};
+
+class TopDownSolution {
+  int knapSack(int capacity, vector<int> &val, vector<int> &weights) {
+    int n = val.size();
+    vector<vector<int>> dp(n + 1, vector<int>(capacity + 1, 0));
+
+    for (int i = 1; i <= n; i++) {
+      for (int wt = 1; wt <= capacity; wt++) {
+        if (weights[i - 1] <= wt) {
+          dp[i][wt] =
+              max(val[i - 1] + dp[i - 1][wt - weights[i - 1]], dp[i - 1][wt]);
+        } else {
+          dp[i][wt] = dp[i - 1][wt];
+        }
+      }
+    }
+    return dp[n][capacity];
+  }
+}
