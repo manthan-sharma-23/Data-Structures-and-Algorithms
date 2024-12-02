b4053f2e75ae2c5629b255a1e85eef6b98a2609d
Manthan Sharma <manthan2306s@gmail.com>
2024-12-02 21:12:05 +0530

    91 leetcode

--- DP/ll-2597.cc
+++ DP/ll-2597.cc
@@ -1,3 +1,7 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <map>
 #include <unordered_map>
 #include <vector>
 
@@ -25,6 +29,46 @@ public:
   int beautifulSubsets(vector<int> &nums, int k) {
     unordered_map<int, int> freq;
     auto ans = takeOrNot(nums, 0, k, freq);
+    return ans - 1;
+  }
+};
+
+class DPSolution {
+  int beautifulSubsets(vector<int> &nums, int k) {
+    unordered_map<int, vector<int>> freq;
+
+    for (auto el : nums) {
+      freq[el % k].push_back(el);
+    }
+
+    int ans = 1;
+
+    for (auto &[mod_val, v] : freq) {
+      sort(v.begin(), v.end());
+      map<int, int> mp;
+
+      for (auto el : v)
+        mp[el]++;
+
+      int prev_el = INT_MIN, prevNotTaken = 1, prevTaken = 0, nowNotTaken = 0,
+          nowTaken = 0;
+
+      for (auto &[el, freq] : mp) {
+        int possible_subsets = pow(2, freq) - 1;
+
+        nowNotTaken = prevNotTaken + prevTaken;
+        if (prev_el + k == el) {
+          nowTaken = prevNotTaken * possible_subsets;
+        } else {
+          nowTaken = (prevTaken + prevNotTaken) * possible_subsets;
+        }
+
+        prev_el = el;
+        prevNotTaken = nowNotTaken;
+        prevTaken = nowTaken;
+      }
+      ans *= (nowNotTaken + nowTaken);
+    }
     return ans - 1;
   }
 };
