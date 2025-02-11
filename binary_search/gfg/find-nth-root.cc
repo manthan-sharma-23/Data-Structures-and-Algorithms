fb73277f15ab4771cda5b4278a9d45cbddf823ce
Manthan Sharma <manthan2306s@gmail.com>
2025-02-11 09:44:14 +0530

    nth root of m

---
+++ binary_search/gfg/find-nth-root.cc
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+  int nthRoot(int n, int m) {
+
+    int low = 1, high = m;
+
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      int power = pow(mid, n);
+
+      if (power == m)
+        return mid;
+
+      if (power > m)
+        high = mid - 1;
+      else
+        low = mid + 1;
+    }
+
+    return -1;
+  }
+};
