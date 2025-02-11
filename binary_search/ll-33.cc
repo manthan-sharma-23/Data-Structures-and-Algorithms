afa06a1096dfdf458a2c01fe3d217af29d307519
Manthan Sharma <manthan2306s@gmail.com>
2025-02-11 09:24:01 +0530

    single element find

--- binary_search/ll-33.cc
+++ binary_search/ll-33.cc
@@ -4,9 +4,29 @@ using namespace std;
 
 class Solution {
 public:
-  int search(vector<int> &nums, int target) { int n = nums.size();
-    
-  
-  
-   }
+  int search(vector<int> &nums, int target) {
+    int n = nums.size();
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+
+      if (nums[mid] == target)
+        return mid;
+
+      if (nums[mid] >= nums[0]) {
+        if (target > nums[mid] || target < nums[0])
+          low = mid + 1;
+        else
+          high = mid - 1;
+      } else {
+        if (target < nums[mid] || target > nums[n - 1])
+          high = mid - 1;
+        else
+          low = mid + 1;
+      }
+    }
+
+    return -1;
+  }
 };
---
+++ binary_search/ll-540.cc
@@ -0,0 +1,24 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int singleNonDuplicate(vector<int> &nums) {
+    int n = nums.size();
+
+    int low = 0, high = n - 1;
+
+    while (low < high) {
+
+      int mid = low + (high - low) / 2;
+
+      if (nums[mid] == nums[mid ^ 1])
+        low = mid + 1;
+      else
+        high = mid;
+    }
+
+    return nums[low];
+  }
+};
